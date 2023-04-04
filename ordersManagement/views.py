524218a284b24b1554dd03772d85f38415866ec2
Rafael Ortiz <rortiz@inkremental.co>
2023-04-03 22:50:24 -0500

    Configure email and send data to an email account with form

--- ordersManagement/views.py
+++ ordersManagement/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from ordersManagement.models import Products
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def search_products(request):
@@ -21,5 +23,11 @@ def search(request):
 
 def contact(request):
     if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+        email = request.POST['email']
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ['rortiz@inkremental.co']
+        send_mail(subject, message, email_from, recipient_list)
         return render(request, 'thanks.html')
     return render(request, 'contact.html')
